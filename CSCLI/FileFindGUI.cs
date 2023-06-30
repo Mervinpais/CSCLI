040389c7bad46ae64701beb0002e4d279e0342ed
Mervinpais14 <mervinpaismakeswindows14@gmail.com>
2023-06-30 07:33:51 +0400

    Added some extra fixes and stuff to fileFindGUI Also has been edited to match the original design

--- CSCLI/FileFindGUI.cs
+++ CSCLI/FileFindGUI.cs
@@ -16,6 +16,9 @@
                     - file2
                     - file3
                 */
+            Console.BackgroundColor = ConsoleColor.DarkGray;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Clear();
             Console.Write("Item Name: ");
             string ItemName = Console.ReadLine();
             Console.WriteLine($"\n=== Files Found in {currentDir} ===");
@@ -29,24 +32,73 @@
                 }
             }
             //Console.WriteLine(string.Join(Environment.NewLine, filesFound.ToArray()));
-            foreach (string file in filesFound)
+            ConsoleKeyInfo consoleKeyInfo = new ConsoleKeyInfo();
+            int selectedLine = 0;
+            int counter = 0;
+            while (consoleKeyInfo.Key != ConsoleKey.Enter)
             {
-                string[] wordInstances = file.Split(ItemName);
-                foreach (string e in wordInstances)
+                Console.Clear();
+                Console.Write($"Item Name: {ItemName}");
+                Console.WriteLine($"\n=== Files Found in {currentDir} ===");
+
+                foreach (string file in filesFound)
                 {
-                    Console.ResetColor();
-                    Console.Write(e);
-                    Console.BackgroundColor = ConsoleColor.White;
-                    Console.ForegroundColor = ConsoleColor.Black;
-                    Console.Write(ItemName);
-                    Console.ResetColor();
+                    string[] wordInstances = file.Split(ItemName);
+                    if (counter == selectedLine)
+                    {
+                        Console.BackgroundColor = ConsoleColor.White;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.Write("> ");
+
+                    }
+                    else
+                    {
+                        Console.BackgroundColor = ConsoleColor.DarkGray;
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    foreach (string e in wordInstances)
+                    {
+                        if (selectedLine != counter) { Console.BackgroundColor = ConsoleColor.DarkGray; Console.ForegroundColor = ConsoleColor.White; }
+
+                        else { Console.BackgroundColor = ConsoleColor.White; Console.ForegroundColor = ConsoleColor.Black; }
+
+                        Console.Write(e);
+
+                        if (selectedLine != counter) { Console.BackgroundColor = ConsoleColor.White; Console.ForegroundColor = ConsoleColor.Black; }
+
+                        else { Console.BackgroundColor = ConsoleColor.Black; Console.ForegroundColor = ConsoleColor.White; }
+
+                        Console.Write(ItemName);
+                        Console.BackgroundColor = ConsoleColor.DarkGray;
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    counter++;
+
+                    Console.SetCursorPosition(Console.CursorLeft - ItemName.Length, Console.CursorTop);
+                    for (int i = 0; i < ItemName.Length; i++)
+                    {
+                        Console.Write(" ");
+                    }
+                    Console.WriteLine();
                 }
-                Console.SetCursorPosition(Console.CursorLeft - ItemName.Length, Console.CursorTop);
-                for (int i = 0; i < ItemName.Length; i++)
+                consoleKeyInfo = Console.ReadKey(true);
+                if (consoleKeyInfo.Key == ConsoleKey.UpArrow)
                 {
-                    Console.Write(" ");
+                    selectedLine--;
+                    if (selectedLine < 0)
+                    {
+                        selectedLine = counter;
+                    }
                 }
-                Console.WriteLine();
+                else if (consoleKeyInfo.Key == ConsoleKey.DownArrow)
+                {
+                    selectedLine++;
+                    if (selectedLine > counter)
+                    {
+                        selectedLine = 0;
+                    }
+                }
+                counter = 0;
             }
         }
     }
